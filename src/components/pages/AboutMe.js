d8ce6e18c032631d409fdbf1a70745a15a723b8a
sebastienvuong <sebastien.vuong.teamcanada@gmail.com>
2017-06-13 17:48:54 +0000

    Made the navigation buttons with mock functionalities

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,9 +4,19 @@ import './AboutMe.css';
 
 class AboutMe extends Component {
     
-    // constructor() {
-    //     super();
-    // }
+    constructor() {
+        super();
+        this.state = {
+            currentSection: 0
+        }
+    }
+    
+    _handleClick = (buttonIdx) => {
+        console.log("clicked", buttonIdx)
+        this.setState({
+            currentSection: buttonIdx
+        })
+    }
     
     componentDidMount = () => {
         this.props.onHomepage(false);
@@ -144,140 +154,50 @@ class AboutMe extends Component {
                 }
             ]
         })
-        console.log(content);
+        // console.log(content);
+        var that = this;
         return (
             <div className="about-me-body">
                 <div className="nav-bar-container">
                     <div className="nav-bar">
-                        {content.map(section=>
-                            <button key={section.title} className="nav-button">
+                        {content.map((section,idx)=>
+                            (<button 
+                                key={section.title} 
+                                className="nav-button"
+                                onClick={that._handleClick.bind(this, idx)}
+                            >
                                 {section.title}
-                            </button>
+                            </button>)
                         )}
                     </div>
                 </div>
-                <div className="main-content gradientback">
+                <div ref="content" className="main-content gradientback">
                     <div className="content-section">
                         {content.map(section=>
-                            <div key={section.title} className="section-title">
+                            (<div key={section.title} className="section-title">
                                 {section.title.toUpperCase()}
                                 {section.entries.map(entry=>
-                                    <div key={entry.title} className="section-entry">
+                                    (<div key={entry.title} className="section-entry">
                                         <span className="entry-title">{entry.title}</span>
                                         <span className="entry-description">
                                             <pre>{entry.subtitle}</pre>
                                         </span>
                                         <ul>
                                         {entry.tasks.map(task=>
-                                            <li key={task} className="entry-task">
+                                            (<li key={task} className="entry-task">
                                                 {task}
-                                            </li>
+                                            </li>)
                                         )}
                                         </ul>
                                         <hr/>
-                                    </div>
+                                    </div>)
                                 )}
-                            </div>
+                            </div>)
                         )}
                     </div>
                 </div>
             </div>
         );
-        /*
-        return (
-            <div className="about-me-body">
-                <div className="nav-bar-container">
-                    <div className="nav-bar">
-                        <button className="nav-button">
-                            My philosophy
-                        </button>
-                        <button className="nav-button">
-                            Education
-                        </button>
-                        <button className="nav-button">
-                            Past experience
-                        </button>
-                        <button className="nav-button">
-                            Other professional experience
-                        </button>
-                        <button className="nav-button">
-                            Projects and awards
-                        </button>
-                    </div>
-                </div>
-                <div className="main-content gradientback">
-                    <div>
-                        Quomodo cecidisti de caelo Lucifer, qui mane oriebaris? Corruisti in terram, qui vulnerabas gentes?
-                    </div>
-                    <div className="content-section">
-                        <div className="section-title">
-                            ## PHILOSOPHY
-                            <div className="section-entry">
-                                In coding, just like in life, we should not limit ourselves to what we know we can do. 
-                                Instead, we should dreaam of how we want it to be. From that point, we can start to look for the means to achieve that ideal vision.
-                            </div>
-                        </div>
-                        <div className="section-title">
-                            ## EDUCATION
-                        </div>
-                        <div className="section-entry">
-                            DecodeMTL : April 2017 - May 2017 <br/>
-                            <span className="entry-description"> 
-                                <pre>Full-Stack Web Development Course</pre> 
-                            </span>
-                        </div>
-                        <div className="section-entry">
-                            École Polytechnique : August 2009 - April 2015 <br/>
-                            <span className="entry-description"> 
-                                <pre>B.Eng in Mechanical Engineering, Analysis & Design orientation (as student-athlete)</pre> 
-                            </span>
-                        </div>
-                    </div>
-                    <div className="content-section">
-                        <div className="section-title">
-                            ## PAST EXPERIENCE
-                        </div>
-                        <div className="section-entry">
-                            Dialogue Technologies – Clinic Management System
-                            <span className="entry-description"> 
-                                <pre>Full-Stack project consisting in an app for patients to book appointments with specialists</pre>
-                            </span>
-                        </div>
-                        <div className="section-entry">
-                            École Polytechnique : August 2009 - April 2015 <br/>
-                            Mechanical Engineering Baccalaureate (as student-athlete)
-                        </div>
-                    </div>
-                    <div className="content-section">
-                        <div className="section-title">
-                            ## PAST EXPERIENCE
-                        </div>
-                        <div className="section-entry">
-                            DecodeMTL : April 2017 - May 2017 <br/>
-                            Full-Stack Web Development Immersive
-                        </div>
-                        <div className="section-entry">
-                            École Polytechnique : August 2009 - April 2015 <br/>
-                            Mechanical Engineering Baccalaureate (as student-athlete)
-                        </div>
-                    </div>
-                    <div className="content-section">
-                        <div className="section-title">
-                            ## PAST EXPERIENCE
-                        </div>
-                        <div className="section-entry">
-                            DecodeMTL : April 2017 - May 2017 <br/>
-                            Full-Stack Web Development Immersive
-                        </div>
-                        <div className="section-entry">
-                            École Polytechnique : August 2009 - April 2015 <br/>
-                            Mechanical Engineering Baccalaureate (as student-athlete)
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-        */
     }
 }
 
